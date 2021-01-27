a898da05ff8f8fd6c409609b4787a798d90f4c79
ZEHRA <z.nuralkan@gmail.com>
2021-01-27 21:40:41 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri m)
+        {
+            Console.WriteLine("Müşteri adı" + m.Ad + "soyadı:" + m.Soyad + "adlı müşteri eklendi");
+        }
+
+        public void Listele()
+        {
+            Console.WriteLine("Müşteri listelendi");
+
+        }
+        public void Sil(Musteri m)
+        {
+            Console.WriteLine(m.Ad+" adlı "+m.Soyad+" soyadlı müşterinin silme islemi başarılı ");
+
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri { Ad = "ZEHRA ", Soyad = "ALKAN ", Id = 1 };
+            Musteri m2 = new Musteri { Ad = "BULUT ", Soyad = "ARAS ", Id = 2 };
+            Musteri m3 = new Musteri { Ad = "BÜŞRA ", Soyad = ". ", Id = 3 };
+            MusteriManager m = new MusteriManager();
+
+            m.Ekle(m1);
+            m.Ekle(m2);
+            m.Ekle(m3);
+            m.Listele();
+            m.Sil(m1);
+          
+        }
+    }
+}
